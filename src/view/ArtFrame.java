cfee237cde1cf539f0dc737cde755e0df1010fa6
Ryan <jlkfop@gmail.com>
2019-05-03 09:32:10 -0600

    Set up the frame

--- src/view/ArtFrame.java
+++ src/view/ArtFrame.java
@@ -1,9 +1,32 @@
 package view;
 
 import controller.ArtController;
+
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class ArtFrame extends JFrame
 {
+	private ArtController app;
+	private ArtPanel appPanel;
 	
+	public ArtFrame(ArtController app)
+	{
+		super();
+		this.app = app;
+		this.appPanel = new ArtPanel(app);
+		
+		setupFrame();
+	}
+	
+	public void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 800);
+		this.setResizable(true);
+		this.setTitle("A 3 year old could do this");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
 }
