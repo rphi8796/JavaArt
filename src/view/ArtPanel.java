7685f16c186ba4c9c09f6e94c051208e81231253
Ryan <jlkfop@gmail.com>
2019-05-03 09:56:06 -0600

    added to the artPanel

--- src/view/ArtPanel.java
+++ src/view/ArtPanel.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SpringLayout;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,9 +19,20 @@ public class ArtPanel extends JPanel
 	private ArtController app;
 	
 	private SpringLayout appLayout;
-	private JButton redButton;
+	
+	private JButton red;
+	private JButton orange;
+	private JButton yellow;
+	private JButton green;
+	private JButton blue;
+	private JButton violet;
+	private JButton random;
+	
+	
 	private JScrollPane scrolly;
+	
 	private ImageIcon duck;
+	
 	private JPanel buttonPanel;
 	
 	
@@ -31,7 +43,13 @@ public class ArtPanel extends JPanel
 		
 		appLayout = new SpringLayout();
 		
-		redButton = new JButton();
+		red = new JButton();
+		orange = new JButton();
+		yellow = new JButton();
+		green = new JButton();
+		blue = new JButton();
+		violet = new JButton();
+		random = new JButton();
 		
 		scrolly = new JScrollPane();
 		
@@ -40,9 +58,24 @@ public class ArtPanel extends JPanel
 		buttonPanel = new JPanel(new GridLayout(1, 0));
 	}
 	
+	public void setupButtons()
+	{
+		buttonPanel.add(red);
+		buttonPanel.add(orange);
+		buttonPanel.add(yellow);
+		buttonPanel.add(green);
+		buttonPanel.add(blue);
+		buttonPanel.add(violet);
+		buttonPanel.add(random);
+	}
+	
 	public void setupPanel()
 	{
+		this.setLayout(appLayout);
 		
+		buttonPanel.setPreferredSize(new Dimension(100, 600));
+		this.add(buttonPanel);
+		this.add(scrolly);
 	}
 	
 	public void setupScrollPane()
