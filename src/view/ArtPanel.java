481d639e6728d2f882f9d3b3c67b76f66def5570
Ryan <jlkfop@gmail.com>
2019-05-03 09:41:14 -0600

    I added most of the imports for JPanel and a couple of the components

--- src/view/ArtPanel.java
+++ src/view/ArtPanel.java
@@ -1,16 +1,42 @@
 package view;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import controller.ArtController;
+
 
 public class ArtPanel extends JPanel
 {
 	private ArtController app;
 	
+	private SpringLayout appLayout;
+	private JButton redButton;
+	private JScrollPane scrolly;
+	private ImageIcon duck;
+	private JPanel buttonPanel;
+	
+	
 	public ArtPanel(ArtController app)
 	{
 		super();
 		this.app = app;
+		
+		appLayout = new SpringLayout();
+		
+		redButton = new JButton();
+		
+		scrolly = new JScrollPane();
+		
+		duck = new ImageIcon();
+		
+		buttonPanel = new JPanel(new GridLayout(1, 0));
 	}
 	
 	public void setupPanel()
