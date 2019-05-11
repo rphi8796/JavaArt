29270752d373f452d0606e8159337a53b0f8a0b0
Ryan <jlkfop@gmail.com>
2019-05-10 22:33:26 -0600

    I added so much but now I am dying

--- src/view/SubPanel.java
+++ src/view/SubPanel.java
@@ -64,22 +64,123 @@ public class SubPanel extends JPanel
 		myGraphics.drawLine(900, 150, 750, 350);
 		myGraphics.drawLine(900, 750, 750, 650);
 		
-		myGraphics.drawLine(0, 0, 300, 150);
-		myGraphics.drawLine(0, 1000, 300, 750);
+
 		
+		//Grid in the middle
 		for(int x = 450; x < 750; x += 10)
 		{
 			myGraphics.drawLine(x, 350, x, 650);
 			myGraphics.drawLine(450, x - 100, 750, x - 100);
 		}
 		
+		//Vertical lines for top trapezoid
 		for(int i = 460, x = -140; i < 750; i += 10, x += 10)
 		{
 			myGraphics.drawLine(i, 350, i + x, 150);
 		}
 		
+		//Vertical lines for bottom trapezoid
+		for(int i = 460, x = -140; i < 750; i += 10, x += 10)
+		{
+			myGraphics.drawLine(i, 650, i + x, 750);
+		}
 		
-
+		//Vertical line for left trap
+		for(double i = 350, y = -200; i < 501; i += 10, y += 13.333333)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			myGraphics.drawLine(300, intI + intY, 450, intI);
+		}
+		for(double i = 500, y = 0; i < 650; i += 10, y += 6.666666)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			myGraphics.drawLine(300, intI + intY, 450, intI);
+		}
+		//end
+		
+		//Vertical line for right trap
+		for(double i = 350, y = -200; i < 501; i += 10, y += 13.333333)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			myGraphics.drawLine(900, intI + intY, 750, intI);
+		}
+		for(double i = 500, y = 0; i < 650; i += 10, y += 6.666666)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			myGraphics.drawLine(900, intI + intY, 750, intI);
+		}
+		//end
+		myGraphics.setColor(Color.CYAN);
+		//Makes the first layer of horizontal lines
+		for(int x = 1; x < 30; x += 1)
+		{
+			int y = (int ) ((.225 * x * x) + (-.255 * x) + 1);
+			int xOne = (int) ((-0.168751 * x * x) + (0.168784 * x) + 449.25);
+			int length = (int) ((0.3375 * x * x) + (-0.3375 * x) + 301.5);
+			length -= 1;
+			xOne += 1;
+			myGraphics.drawRect(xOne, 350 - y, length, length);
+//			myGraphics.drawLine(xOne, 350 - y , xTwo, 350 - y);
+//			myGraphics.drawLine(xOne, 150, xOne, 750);
+		}
+		
+		
+		myGraphics.setColor(Color.WHITE);
+		myGraphics.drawLine(0, 0, 300, 150);
+		myGraphics.drawLine(0, 1000, 300, 750);
+		myGraphics.drawLine(1200, 1000, 900, 750);
+		myGraphics.drawLine(1200, 0, 900, 150);
+		//Vertical lines for top top trapezoid
+		for(int i = 320, x = -280; i < 900; i += 20, x += 20)
+		{
+			myGraphics.drawLine(i, 150, i + x, 0);
+		}
+		
+		//Vertical lines for bottom bottom trapezoid
+		for(int i = 320, x = -280; i < 900; i += 20, x += 20)
+		{
+			myGraphics.drawLine(i, 750, i + x, 1000);
+		}
+		
+		//Vertical line for side trap bottoms
+		for(double i = 350, y = -200, shift = -150; i < 501; i += 10, y += 13.333333, shift += 10)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			int intShift = (int) shift;
+			myGraphics.drawLine(0, intI + intY + intShift, 300, intI+ intY);
+		}
+		for(double i = 500, y = 0, shift = 0; i < 650; i += 10, y += 6.666666, shift += 15)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			int intShift = (int) shift;
+			myGraphics.drawLine(0, intI + intY + intShift, 300, intI + intY);
+			
+		}
+		//end
+		
+		//Vertical line for side trap bottoms
+		for(double i = 350, y = -200, shift = -150; i < 501; i += 10, y += 13.333333, shift += 10)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			int intShift = (int) shift;
+			myGraphics.drawLine(1200, intI + intY + intShift, 900, intI+ intY);
+		}
+		for(double i = 500, y = 0, shift = 0; i < 650; i += 10, y += 6.666666, shift += 15)
+		{
+			int intI = (int) i;
+			int intY = (int) y;
+			int intShift = (int) shift;
+			myGraphics.drawLine(1200, intI + intY + intShift, 900, intI + intY);
+			
+		}
+		
 		
 	}
 	
